09295700ff2d1c605abb744fddd8de59977baadc
Alumno <ulsa@iMacD114.local>
2018-10-09 18:55:16 -0700

    hola

--- Cafe/ViewController.swift
+++ Cafe/ViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUsuario: UITextField!
+    @IBOutlet weak var txtContraseña: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        txtUsuario.layer.borderWidth = 1
+        txtUsuario.layer.borderColor = UIColor.white.cgColor
+        txtUsuario.layer.cornerRadius = 20
+        txtUsuario.clipsToBounds = true
+        
+        txtContraseña.layer.borderWidth = 1
+        txtContraseña.layer.borderColor = UIColor.white.cgColor
+        txtContraseña.layer.cornerRadius = 20
+        txtContraseña.clipsToBounds = true
+        
+        btnLogin.layer.cornerRadius = 20
+        btnLogin.clipsToBounds = true
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
+        
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
